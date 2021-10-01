a0fb92937bcb7995a9fd606334d3f3689d709114
Shahzeb Siddiqui <shahzebsiddiqui@lbl.gov>
2021-10-01 18:49:49 +0000

    Update setup.sh, .gitlab-ci.yml files

---
+++ setup.sh
@@ -0,0 +1,5 @@
+export EASYBUILD_MODULE_SYNTAX=Tcl
+export EASYBUILD_MODULES_TOOL=EnvironmentModulesC
+export EASYBUILD_PREFIX=/global/common/software/nersc/easybuild/cori/21.02
+export EASYBUILD_SOURCEPATH=/global/common/software/nersc/easybuild/sources
+export EASYBUILD_OPTARCH=haswell
