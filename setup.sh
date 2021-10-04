9daa47d460559ba535ff811c49ca9993642696b4
Shahzeb Siddiqui <shahzebsiddiqui@lbl.gov>
2021-10-04 21:00:24 +0000

    Update setup.sh, README.md files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-export EASYBUILD_MODULE_SYNTAX=Tcl
-export EASYBUILD_MODULES_TOOL=EnvironmentModulesC
-export EASYBUILD_PREFIX=/global/common/software/nersc/easybuild/cori/21.10
+export EASYBUILD_MODULES_TOOL=Lmod
+export EASYBUILD_PREFIX=/global/common/software/nersc/easybuild/perlmutter/21.10
 export EASYBUILD_SOURCEPATH=/global/common/software/nersc/easybuild/sources
-export EASYBUILD_OPTARCH=haswell
