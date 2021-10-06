ea84cfac0c909873b3e051a74138adacda402fb7
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2021-10-06 07:14:16 -0700

    add perlmutter configuration and remote setup.sh

--- setup.sh
+++
@@ -1,3 +0,0 @@
-export EASYBUILD_MODULES_TOOL=Lmod
-export EASYBUILD_PREFIX=/global/common/software/nersc/easybuild/perlmutter/21.10
-export EASYBUILD_SOURCEPATH=/global/common/software/nersc/easybuild/sources
